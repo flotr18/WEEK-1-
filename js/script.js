269d95d1ee2f1b969a8918b8489bceb77d513298
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-09-21 20:02:24 +0100

    Updated Website

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let vue = new Vue ({
+    el : '#main',
+    data : {
+        title : 'My Website',
+        gif : 'https://media2.giphy.com/media/Nx0rz3jtxtEre/giphy.gif?cid=ecf05e47t6e77t8jj4gjcooi308r6g3wp714zz2m0jnwud6b&rid=giphy.gif'
+    }
+
+});
