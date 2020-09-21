26467e46bd0be59384b036656d53bbce25a3953f
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-09-21 20:13:40 +0100

    Updated

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let vue = new Vue ({
-    el : '#main',
+    el : '#container',
     data : {
         title : 'My Website',
         gif : 'https://media2.giphy.com/media/Nx0rz3jtxtEre/giphy.gif?cid=ecf05e47t6e77t8jj4gjcooi308r6g3wp714zz2m0jnwud6b&rid=giphy.gif'
