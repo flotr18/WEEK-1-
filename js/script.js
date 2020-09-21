732205dec633f2cef5d406d9d0786303b36f3672
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-09-21 20:43:56 +0100

    Prepered About & Contact

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ let vue = new Vue ({
     el : '#container',
     data : {
         title : 'My Website',
-        gif : 'https://media2.giphy.com/media/Nx0rz3jtxtEre/giphy.gif?cid=ecf05e47t6e77t8jj4gjcooi308r6g3wp714zz2m0jnwud6b&rid=giphy.gif'
+        gif : 'https://media2.giphy.com/media/Nx0rz3jtxtEre/giphy.gif?cid=ecf05e47t6e77t8jj4gjcooi308r6g3wp714zz2m0jnwud6b&rid=giphy.gif',
+
     }
 
 });
