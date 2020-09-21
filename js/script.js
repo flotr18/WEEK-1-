40ff5c3702d66c74ecb636c99d77debb7ec149c1
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-09-22 00:44:14 +0100

    Add some stuffs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-let vue = new Vue ({
+let home = new Vue ({
     el : '#container',
     data : {
         title : 'My Website',
         gif : 'https://media2.giphy.com/media/Nx0rz3jtxtEre/giphy.gif?cid=ecf05e47t6e77t8jj4gjcooi308r6g3wp714zz2m0jnwud6b&rid=giphy.gif',
 
+
     }
 
 });
+
+
+let about = new Vue ({
+    el : '#AboutContainer',
+    data : {
+      id : 'ID Details',
+        infos : [
+            'Name : Florent',
+            'Family Name : Trouillet',
+            'Date of birth : 18/07/1197',
+            'Area of Study : Web Development'
+        ],
+        hobbies : 'My hobbies',
+        cards : [
+            'Aeronautical Engineering',
+            'Gliding',
+            'Digital Transformation',
+            'Technology',
+            'SF Movies & Series',
+            'History',
+            'Anglo-Saxon culture',
+            'Thriller Novels'
+        ]
+
+    }
+
+});
+
